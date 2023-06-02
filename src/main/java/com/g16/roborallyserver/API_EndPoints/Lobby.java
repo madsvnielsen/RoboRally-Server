1e192af219ce0e47b9376446d0cb69ba46191e58
MathiasW <s224293@student.dtu.dk>
2023-06-02 12:55:53 +0200

    Made a list of lobbies

--- src/main/java/com/g16/roborallyserver/API_EndPoints/Lobby.java
+++ src/main/java/com/g16/roborallyserver/API_EndPoints/Lobby.java
@@ -8,21 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @RequestMapping(value = "/lobby", produces="application/json")
 @RestController
 public class Lobby {
     GameSessionManager gsm = new GameSessionManager();
-    @GetMapping(value ="/")
+    @GetMapping(value ="")
     public ResponseEntity<?> lobbies(){
         return new ResponseEntity<>(gsm.getGameSessions(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/{gameID}")
-    public ResponseEntity<?> lobby(String gameID){
-        Optional<GameSession> queryResponse = gameSessions.stream().filter(gs -> gs.gameID.equals(gameID)).findFirst();
+    public GameSession lobby(String gameID){
+        Optional<GameSession> queryResponse = gsm.getGameSessions().stream().filter(gs -> gs.gameID.equals(gameID)).findFirst();
         return queryResponse.orElse(null);
     }
 }
